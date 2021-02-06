80236d8526ee81f4cc87273367ba052166ab21c6
Richard van der Weerd <richard.vd.weerd@gmail.com>
2021-02-06 10:10:47 +0100

    Added an external train definition file

---
+++ src/traininfo.h
@@ -0,0 +1,30 @@
+#ifndef TRAINSETTINGS.H
+#define TRAINSETTINGS
+
+
+// select one of the trains
+#define TRAIN_942_FRONT
+// #define TRAIN_942_BACK
+// #define TRAIN_RW_1617_ASSEN
+// #define TRAIN_RW_1638_GRONINGEN
+
+#ifdef TRAIN_942_FRONT
+#define LOK_ADDRESS 9
+#define NUM_PIXELS 9
+#define CAB_LIGHT       // defines the last pixel to be a cab-light, comment out to use all NUM_PIXELS for flickering lights
+//#define FRONT           // determines whether to show front or back light when driving forward and vice versa, default = FRONT
+#endif // TRAIN_942_FRONT
+
+#ifdef TRAIN_942_BACK
+#define LOK_ADDRESS 9
+#define NUM_PIXELS 9
+#define CAB_LIGHT       // defines the last pixel to be a cab-light, comment out to use all NUM_PIXELS for flickering lights
+#define BACK           // determines whether to show front or back light when driving forward and vice versa
+#endif // TRAIN_942_FRONT
+
+
+// #ifdef TRAIN_RW_1617_ASSEN
+
+// #endif // TRAIN_RW_1617_ASSEN
+
+#endif
