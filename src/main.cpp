41d31a25f7632c643f142ba429f4a58f3e704a8a
Richard van der Weerd <richard.vd.weerd@gmail.com>
2021-02-07 11:42:22 +0100

    working version with external train-settings

--- src/main.cpp
+++ src/main.cpp
@@ -30,8 +30,9 @@ const uint8_t  neoPin = PB0;
     const bool cab_light = false;
 #endif
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_PIXELS, neoPin, NEO_GRB + NEO_KHZ800);
-FluorescentLights lights[numPixels - (cab_light ? 1 : 0)]; // use 1 less pixel for flickering lights when there is a cab light
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(numPixels, neoPin, NEO_GRB + NEO_KHZ800);
+//FluorescentLights lights[numPixels - (cab_light ? 1 : 0)]; // use 1 less pixel for flickering lights when there is a cab light
+FluorescentLights lights[numPixels]; // use 1 less pixel for flickering lights when there is a cab light
 
 void setup() {
   // pinMode(neoPin, OUTPUT); // is set in strip.begin()
@@ -55,7 +56,7 @@ void setup() {
 
   // setup lights
   strip.begin();
-  for (uint16_t i = 0; i < NUM_PIXELS; i++)
+  for (uint16_t i = 0; i < numPixels; i++)    // set up all pixels, including the cabin lights, if there's one
     strip.setPixelColor(i, 0,0,0);
   strip.show();
 
@@ -80,45 +81,58 @@ void loop() {
   static bool forceOff=true;
   static bool cabinLight = false;
   static bool stripChanged = true;
-  int lightsOn = (int) dccGetFunction(4);
+  int lightsOn = (int) dccGetFunction(FL_LIGHT_FUNCTION);
   uint8_t dir = dccGetDirection();
   uint8_t speed = dccGetSpeed();
   uint8_t f0 = dccGetFunction(0);
 
-  uint16_t countOn = 0;
-  // loop through all pixels except cabin light
-  for (uint16_t i = 0; i < numPixels - (cab_light ? 1 : 0); i++)
-  {
-      lights[i].switchLight(lightsOn); 
-      
-      // r, g, b = 22,21,18
-      strip.setPixelColor(i /* *stepValue*/, lights[i]._state * 22, lights[i]._state * 21, lights[i]._state * 18);
+  /****************************************************************************************************
+   * 
+   * handle front and tail lights
+   * 
+   ****************************************************************************************************/
+  if (f0) 
+  { 
+    // light is on
+    if (dir)
+    {
+      #ifdef TAIL
+        digitalWrite(PIN_FORWARD, LOW);
+        digitalWrite(PIN_BACKWARD, HIGH);
+      #else
+        digitalWrite(PIN_FORWARD, HIGH); 
+        digitalWrite(PIN_BACKWARD, LOW);
+      #endif
+    }
+    else
+    {
+      #ifdef TAIL
+        digitalWrite(PIN_FORWARD, HIGH); 
+        digitalWrite(PIN_BACKWARD, LOW);
+      #else
+        digitalWrite(PIN_FORWARD, LOW);
+        digitalWrite(PIN_BACKWARD, HIGH);
+      #endif
+    }
+  }
+  else
+  {  // light is off
+    digitalWrite(PIN_FORWARD, LOW);
+    digitalWrite(PIN_BACKWARD, LOW);
+  }
 
-      if (lights[i]._on)
-        countOn++;
-  }
-  // if((lightsOn && (countOn < ((numPixels+1)/2))) || (!lightsOn && (countOn > 0)))
-  if (lightsOn && !forceOff)
-  {
-    //strip.show();
-    stripChanged = true;
-    if (countOn ==  numPixels)
-      forceOff = true;
-  }
-  if ((!lightsOn && forceOff))
-  {
-    //strip.show();
-    stripChanged = true;
-    forceOff = false;
-  }
-
-  #ifdef CAB_LIGHT
+  /****************************************************************************************************
+   * 
+   * handle cabin light
+   * 
+   ****************************************************************************************************/
+  #ifdef CAB_LIGHT      // we can skip this entire code if there is not a cabin light
   // handle cabin light
-  if (f0 && dir && speed < 2)
+  if (f0 && dir && speed <= CAB_ON_MAX_SPEED)
   {  // turn cabin light on!
     if (!cabinLight)
     {  // light was off, turn on
-      strip.setPixelColor(numPixels-1, 5,5,5);
+      strip.setPixelColor(numPixels-1, CAB_RED, CAB_GREEN, CAB_BLUE);
       cabinLight = true;
       stripChanged = true;
     }
@@ -134,45 +148,44 @@ void loop() {
   }
   #endif // CAB_LIGHT
 
+  /****************************************************************************************************
+   * 
+   * handle fluorenscent lights
+   * 
+   ****************************************************************************************************/
+  uint16_t countOn = 0;
+  // loop through all pixels except cabin light
+  for (uint16_t i = 0; i < numPixels - (cab_light ? 1 : 0); i++)
+  {
+      lights[i].switchLight(lightsOn); 
+      
+      // r, g, b = 22,21,18
+      strip.setPixelColor(i /* *stepValue*/, lights[i]._state * FL_RED, lights[i]._state * FL_GREEN, lights[i]._state * FL_BLUE);
 
+      if (lights[i]._on)
+        countOn++;
+  }
+  // if((lightsOn && (countOn < ((numPixels+1)/2))) || (!lightsOn && (countOn > 0)))
+  if (lightsOn && !forceOff)
+  {
+    //strip.show();
+    stripChanged = true;
+    if (countOn ==  numPixels - (cab_light ? 1 : 0)) // minus the cabine light, if there is one
+      forceOff = true;
+  }
+  if ((!lightsOn && forceOff))
+  {
+    //strip.show();
+    stripChanged = true;
+    forceOff = false;
+  }
+
+  // handle changes to the strip
   if (stripChanged)
   {
     strip.show();
     stripChanged = false;
   }
-
-  // handle front and rear lights
-  if (f0) 
-  { 
-    // light is on
-    if (dir)
-    {
-      #ifdef BACK
-        digitalWrite(PIN_FORWARD, LOW);
-        digitalWrite(PIN_BACKWARD, HIGH);
-      #else
-        digitalWrite(PIN_FORWARD, HIGH); 
-        digitalWrite(PIN_BACKWARD, LOW);
-      #endif
-    }
-    else
-    {
-      #ifdef BACK
-        digitalWrite(PIN_FORWARD, HIGH); 
-        digitalWrite(PIN_BACKWARD, LOW);
-      #else
-        digitalWrite(PIN_FORWARD, LOW);
-        digitalWrite(PIN_BACKWARD, HIGH);
-      #endif
-    }
-  }
-  else
-  {  // light is off
-    digitalWrite(PIN_FORWARD, LOW);
-    digitalWrite(PIN_BACKWARD, LOW);
-  }
 }
 
 
-
-// todo: test F13 - F28
--- src/trainsettings.h
+++ src/trainsettings.h
@@ -2,25 +2,59 @@
 #define TRAINSETTINGS_H
 
 
+/******************************************************************
+ * Definition file for the train settings
+ * 
+ * #define LOK_ADDRESS       3   // defines the dcc lok address
+ * #define FL_LIGHT_FUNCTION 3   // defines the function where the light are connected to
+ * #define NUM_PIXELS        9   // defines the total number of pixels, thus including the cabin light
+ * #define CAB_LIGHT             // defines the last pixel as the cab_light, comment out if there is no cabin light
+ * #define CAB_ON_MAX_SPEED  2   // defines max speed when light in cabine turns on; when 0 light turns on only at stop
+ * #define TAIL                  // defines this is the back of the train, reversing front and tail lights, comment out when front
+ * #define FL_RED            22  // RGB-codes for the fluorescent lights 
+ * #define FL_GREEN          21
+ * #define FL_BLUE           18
+ * #define CAB_RED           5   // RGB-codes for the cabin
+ * #define CAB_GREEN         5
+ * #define CAB_BLUE          5
+ * 
+ *********************************************************************/
+
 // select one of the trains
 #define TRAIN_942_FRONT
 // #define TRAIN_942_BACK
 // #define TRAIN_RW_1617_ASSEN
 // #define TRAIN_RW_1638_GRONINGEN
 
+// 942 is a MAT'64 electric passenger train, with 2 (motorized) carriages
 #ifdef TRAIN_942_FRONT
-    #define LOK_ADDRESS 9
-    #define NUM_PIXELS 9
-    #define CAB_LIGHT           // defines the last pixel to be a cab-light, comment out to use all NUM_PIXELS for flickering lights
-    #define CAB_ON_MAX_SPEED 2  // max speed when light in cabine turns on; when 0 light turns on only at stop
-    //#define FRONT             // determines whether to show front or back light when driving forward and vice versa, default = FRONT
+    #define LOK_ADDRESS         942 // defines the dcc lok address
+    #define FL_LIGHT_FUNCTION   3   // defines the function where the light are connected to
+    #define NUM_PIXELS          9   // defines the total number of pixels, thus including the cabin light
+    #define CAB_LIGHT               // defines the last pixel to be a cab-light, comment out to use all NUM_PIXELS for flickering lights
+    #define CAB_ON_MAX_SPEED    2   // max speed when light in cabine turns on; when 0 light turns on only at stop
+    #define TAIL                    // determines whether to show front or back light when driving forward and vice versa, default = FRONT
+    #define FL_RED              22  // RGB-codes for the fluorescent lights 
+    #define FL_GREEN            21
+    #define FL_BLUE             18
+    #define CAB_RED             5   // RGB-codes for the cabin
+    #define CAB_GREEN           5
+    #define CAB_BLUE            5
 #endif // TRAIN_942_FRONT
 
 #ifdef TRAIN_942_BACK
-    #define LOK_ADDRESS 9
-    #define NUM_PIXELS 9
-    // #define CAB_LIGHT        // defines the last pixel to be a cab-light, comment out to use all NUM_PIXELS for flickering lights
-    #define BACK                // determines whether to show front or back light when driving forward and vice versa
+    #define LOK_ADDRESS         9   // defines the dcc lok address
+    #define FL_LIGHT_FUNCTION   3   // defines the function where the light are connected to
+    #define NUM_PIXELS          9   // defines the total number of pixels, thus including the cabin light
+    #define CAB_LIGHT               // defines the last pixel to be a cab-light, comment out to use all NUM_PIXELS for flickering lights
+    #define CAB_ON_MAX_SPEED    2   // max speed when light in cabine turns on; when 0 light turns on only at stop
+    #define TAIL                    // determines whether to show front or back light when driving forward and vice versa, default = FRONT
+    #define FL_RED              22  // RGB-codes for the fluorescent lights 
+    #define FL_GREEN            21
+    #define FL_BLUE             18
+    #define CAB_RED             5   // RGB-codes for the cabin
+    #define CAB_GREEN           5
+    #define CAB_BLUE            5
 #endif // TRAIN_942_FRONT
 
 
